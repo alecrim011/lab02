8a67b57334d6710d597373348b52ca0c5abfec3e
IAGO ALECRIM LEITE 115211549 <iagoal@lcc2-32.lcc.ufcg.edu.br>
2017-05-25 11:27:46 -0300

    primeiro commit

---
+++ Contato.java
@@ -0,0 +1,76 @@
+package lab02;
+
+public class Contato {
+	private String nome;
+	private String sobrenome;
+	private String telefone;
+	
+	public Contato(String nome, String sobrenome, String telefone){
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,54 @@
+package lab02;
+
+import java.util.Scanner;
+
+public class Main {
+	Contato []contatos = new Contato[100];
+	Scanner scanner = new Scanner(System.in);
+	public static void main (String []args){
+		
+		
+		System.out.println("(C)adastrar Contato" + System.lineSeparator() + "(L)istar Contatos" + System.lineSeparator() + "(E)xibir Contato" + System.lineSeparator() + "(S)air" + System.lineSeparator() + "Opção>");
+		Scanner scanner = new Scanner(System.in);
+		String opcao = scanner.nextLine();
+		String opcaoString;
+		switch (opcao) {
+		case "C": opcaoString = "C";
+		break;
+		
+		case "L": opcaoString = "L";
+		break;
+		
+		case "E": opcaoString = "E";
+		break;
+		
+		case "S": opcaoString = "S";
+		break;
+		
+		default: opcaoString = "OPCAO INVALIDA";
+		     break;
+		}
+		if (opcaoString.equals("C")){
+			
+		}
+		
+		
+		System.out.println(opcaoString);
+	}
+	
+	public void cadastro(){
+		System.out.println("posicao: ");
+		int posicao = scanner.nextInt();
+		System.out.println("nome: ");
+		String nome = scanner.nextLine();
+		System.out.println("sobrenome: ");
+		String sobrenome = scanner.nextLine();
+		System.out.println("telefone: ");
+		String telefone = scanner.nextLine();
+		Contato c = new Contato(nome, sobrenome, telefone);
+		contatos[posicao] = c;
+		
+		
+	}
+
+}
